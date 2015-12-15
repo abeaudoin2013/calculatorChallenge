3dc023be2fef7af81ad84e144500a1e0db19355e
Andy <abeaudoin2013@gmail.com>
2015-12-14 20:33:58 -0500

    made some more steps on the javascript portion. Still unsure about how to get correct number of values to arithmatic methods

--- calculator.js
+++ calculator.js
@@ -10,12 +10,118 @@
 
 // }
 
+
+
 var calculator = {
 
-	"+": function (args) {
+	// arrOfEquations: [],
 
-		return args
+	regExForPar: /\((.+?)\)/,
+
+	eqAsArr: [],
+
+	partsOfEquation: {},
+
+	init: function (eqAsString) {
+
+		this.eqAsArr = eqAsString.split(this.regExForPar);
+
+		this.loopThroughArr();
+
+		this.loopThroughObj();
+		
+		this.loopThroughObjItem();		
+
+	},
+
+	loopThroughArr: function () {
+
+		for (var i=0; i <= this.eqAsArr.length; i++){
+
+			if (this.eqAsArr[i] === "") {
+
+				// console.log('empty string');
+
+			} else if (this.eqAsArr[i] === undefined) {
+
+				// console.log('nothing here')
+
+			} else {
+
+				this.partsOfEquation[i] = this.eqAsArr[i];
+
+			}
+
+		}
+
+	},
+
+	loopThroughObj: function () {
+
+		for (var property in this.partsOfEquation) {
+
+			if (this.partsOfEquation.hasOwnProperty(property)) {
+
+				//property item gets its value (a string) split
+
+				this.partsOfEquation[property] = this.partsOfEquation[property].split('');
+
+			}
+
+		}
+
+	},
+
+	loopThroughObjItem: function () {
+
+		var self = this;
+
+
+
+		for (var property in this.partsOfEquation) {
+
+			if (this.partsOfEquation.hasOwnProperty(property)) {
+
+				for (var i= 0; i <= this.partsOfEquation[property].length; i ++) {
+
+					var onlyNum = [];
+
+					if (this.partsOfEquation[property][i] === "+") {
+
+						// this.plus(onlyNum);
+
+					} else {
+
+						var parsedInt = parseInt(self.partsOfEquation[property][i]);
+
+						onlyNum.push(parsedInt);
+
+					}
+
+					console.log(onlyNum);
+					// if (parseInt(self.partsOfEquation[property][i]) === Number) {
+
+					// 	console.log(self.partsOfEquation[property][i]);
+
+					// } else if (self.partsOfEquation[property][i] === NaN) {
+
+					// 	console.log('cheese');
+					// }
+
+				}
+
+			}
+
+		}
+
+	},
+
+	plus: function (args) {
+
+		console.log('oh, hello');
 
 	}
 
 }
+
+calculator.init('(2+3)(2=2)');
