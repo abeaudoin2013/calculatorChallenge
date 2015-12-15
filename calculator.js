f667b091ba293e103550d9ad93f9460dd84704d9
Andy <abeaudoin2013@gmail.com>
2015-12-15 18:13:24 -0500

    Spent way too long trying to break equation up using mapping, but decided to use ruby eval method and scan user input. Working on writing writing regex

--- calculator.js
+++ calculator.js
@@ -10,118 +10,177 @@
 
 // }
 
+// var calculator = [{}]
 
 
-var calculator = {
 
-	// arrOfEquations: [],
+var claculator = function (eq_as_string) {
 
-	regExForPar: /\((.+?)\)/,
+	eq_as_string
 
-	eqAsArr: [],
+};
 
-	partsOfEquation: {},
 
-	init: function (eqAsString) {
 
-		this.eqAsArr = eqAsString.split(this.regExForPar);
 
-		this.loopThroughArr();
 
-		this.loopThroughObj();
+// var calculator = {
+
+// 	// arrOfEquations: [],
+
+// 	regExForPar: /\((.+?)\)/,
+
+// 	eqAsArr: [],
+
+// 	partsOfEquation: {},
+
+// 	chunksOfEquation: {},
+
+// 	init: function (eqAsString) {
+
+// 		this.eqAsArr = eqAsString.split('');
+
+// 		// loop through eqAsArr and set each key = index item
+
+// 		this.loopThroughArr();
+
+// 		this.mapArr();
+
+// 		// this.eqAsArr.push(this.partsOfEquation);
+
+// 		// this.mapEqAsArr();
+
+// 	},
+
+// 	// mapEqAsArr: function () {
+
+// 	// 	var self = this;
+
+// 	// 	this.eqAsArr.map(function(partsOfEquation) {
+
+// 	// 		if (isNaN(parseInt(partsOfEquation))) {
+				
+// 	// 			console.log('not a number')
+
+// 	// 		} else {
+
+// 	// 			self.partsOfEquation = parseInt(partsOfEquation);
+
+// 	// 		}
+
+// 	// 	})
+
+// 	// },
+
+// 	loopThroughArr: function () {
+
+// 		// this.
+
+// 		for (var i=0; i <= this.eqAsArr.length; i++){
+
+// 			if (this.eqAsArr[i] === "") {
+
+// 				// console.log('empty string');
+
+// 			} else if (this.eqAsArr[i] === undefined) {
+
+// 				// console.log('nothing here')
+
+// 			} else if (isNaN(parseInt(this.eqAsArr[i]))) {
+
+// 				this.eqAsArr[i] = this.eqAsArr[i];
+
+// 			} else {
+
+// 				this.eqAsArr[i] = parseInt(this.eqAsArr[i]);
+
+// 			}
+
+// 		}
+
+// 	},
+
+// 	mapArr: function () {
+
+// 		this.eqAsArr.map(function(value) {
+
+// 			if (value === "(") {
+
+// 				console.log(value);
+
+// 			}
+
+// 		})
+
+// 	},
+
+
+
+// }
+
+// calculator.init('(2+3)(2=2)');
+
+
+		// this.eqAsArr = eqAsString.split(this.regExForPar);
+
+		// this.loopThroughArr();
+
+		// this.loopThroughObj();
 		
-		this.loopThroughObjItem();		
+		// this.loopThroughObjItem();		
 
-	},
 
-	loopThroughArr: function () {
+// 	
 
-		for (var i=0; i <= this.eqAsArr.length; i++){
+// 	loopThroughObjItem: function () {
 
-			if (this.eqAsArr[i] === "") {
-
-				// console.log('empty string');
-
-			} else if (this.eqAsArr[i] === undefined) {
-
-				// console.log('nothing here')
-
-			} else {
-
-				this.partsOfEquation[i] = this.eqAsArr[i];
-
-			}
-
-		}
-
-	},
-
-	loopThroughObj: function () {
-
-		for (var property in this.partsOfEquation) {
-
-			if (this.partsOfEquation.hasOwnProperty(property)) {
-
-				//property item gets its value (a string) split
-
-				this.partsOfEquation[property] = this.partsOfEquation[property].split('');
-
-			}
-
-		}
-
-	},
-
-	loopThroughObjItem: function () {
-
-		var self = this;
+// 		var self = this;
 
 
 
-		for (var property in this.partsOfEquation) {
+// 		for (var property in this.partsOfEquation) {
 
-			if (this.partsOfEquation.hasOwnProperty(property)) {
+// 			if (this.partsOfEquation.hasOwnProperty(property)) {
 
-				for (var i= 0; i <= this.partsOfEquation[property].length; i ++) {
+// 				for (var i= 0; i <= this.partsOfEquation[property].length; i ++) {
 
-					var onlyNum = [];
+// 					var onlyNum = [];
 
-					if (this.partsOfEquation[property][i] === "+") {
+// 					if (this.partsOfEquation[property][i] === "+") {
 
-						// this.plus(onlyNum);
+// 						// this.plus(onlyNum);
 
-					} else {
+// 					} else {
 
-						var parsedInt = parseInt(self.partsOfEquation[property][i]);
+// 						var parsedInt = parseInt(self.partsOfEquation[property][i]);
 
-						onlyNum.push(parsedInt);
+// 						onlyNum.push(parsedInt);
 
-					}
+// 					}
 
-					console.log(onlyNum);
-					// if (parseInt(self.partsOfEquation[property][i]) === Number) {
+// 					console.log(onlyNum);
+// 					// if (parseInt(self.partsOfEquation[property][i]) === Number) {
 
-					// 	console.log(self.partsOfEquation[property][i]);
+// 					// 	console.log(self.partsOfEquation[property][i]);
 
-					// } else if (self.partsOfEquation[property][i] === NaN) {
+// 					// } else if (self.partsOfEquation[property][i] === NaN) {
 
-					// 	console.log('cheese');
-					// }
+// 					// 	console.log('cheese');
+// 					// }
 
-				}
+// 				}
 
-			}
+// 			}
 
-		}
+// 		}
 
-	},
+// 	},
 
-	plus: function (args) {
+// 	plus: function (args) {
 
-		console.log('oh, hello');
+// 		console.log('oh, hello');
 
-	}
+// 	}
 
-}
+// }
 
-calculator.init('(2+3)(2=2)');
--- calculator.rb
+++ calculator.rb
@@ -2,101 +2,289 @@ class Calculator
 
 	attr_accessor :eq_as_string
 
-	def initialize(equation)
+	def initialize
 
-		self.eq_as_string = equation.squeeze(" ").strip
+		puts 'Welcome to Calculator. Please enter your equation:'
 
-		split_string_for_parantheses(self.eq_as_string)
+		equation = gets
 
-	end
+		self.eq_as_string = equation
 
-	def split_string_for_parantheses(eq)
+		test_results = tests
 
-		#split the equation and put everything into array
+		evaluate_string(test_results)
 
-		#because we first we need to find which parts of the equation
-		#are in parantheses
-
-		#regex is looking for parathenses, lazily, and abstracting everything in it until the next parantheses
-
-		eq_as_array = eq.split(%r{\((.+?)\)})
-		map_array(eq_as_array)
+		# tests
 
 	end
 
-	# def split_string_for_exponents
+	def evaluate_string(equation)
+
+		puts 'answer is: '
+
+		puts eval(equation)
+
+	end
+
+	def tests
+
+		# if eq_as_string.scan(/[^)*]/) == [] || eq_as_string.scan(/[^\*\(]/) == []
+
+		# 	puts 'hello'
+
+			# if eq_as_string.scan(/\)\(/) != []
+
+		self.eq_as_string.gsub(/ = /, '==' )
+
+		self.eq_as_string.gsub(/ \)\( / ,')*(')
+
+
+
+			# end
+			
+		# else
+
+			# puts 'Your input is invalid. You must put a * between ) and the number'
+
+			# initialize
+
+		# end
+
+		
+
+		#check it there are any numbers that are butted up against
+		# parantheses on either side of parantheses
+
+		# if self.eq_as_string.gsub(/\)[1]/, ')*1')
+
+		# 	self.eq_as_string.gsub(/\)[1]/, ')*1')
+
+		# end
+
+		# if self.eq_as_string.gsub(/\)[2]/, ')*2')
+
+		# 	self.eq_as_string.gsub(/\)[2]/, ')*2')
+
+		# end
+
+		# if self.eq_as_string.gsub(/\)[3]/, ')*3')
+
+		# 	self.eq_as_string.gsub(/\)[3]/, ')*3')
+
+		# if self.eq_as_string.gsub(/\)[4]/, ')*4')
+
+		# 	self.eq_as_string.gsub(/\)[4]/, ')*4')
+
+		# if self.eq_as_string.gsub(/\)[5]/, ')*5')
+
+		# 	self.eq_as_string.gsub(/\)[5]/, ')*5')
+
+		# if self.eq_as_string.gsub(/\)[6]/, ')*6')
+
+		# 	self.eq_as_string.gsub(/\)[6]/, ')*6')
+
+		# if self.eq_as_string.gsub(/\)[7]/, ')*7')
+
+		# 	self.eq_as_string.gsub(/\)[7]/, ')*7')
+
+		# if self.eq_as_string.gsub(/\)[8]/, ')*8')
+
+		# 	self.eq_as_string.gsub(/\)[8]/, ')*8')
+
+		# if self.eq_as_string.gsub(/\)[9]/, ')*9')
+
+		# 	self.eq_as_string.gsub(/\)[9]/, ')*9')
+
+		# if self.eq_as_string.gsub(/\)[0]/, ')*0')
+
+		# 	self.eq_as_string.gsub(/\)[0]/, ')*0')
+
+		# end
+			
+			
+			
+		# self.eq_as_string.gsub(/\)[2]/, ')*2')
+
+		# self.eq_as_string.gsub(/\)[3]/, ')*3')
+
+		# self.eq_as_string.gsub(/\)[4]/, ')*4')
+
+		# self.eq_as_string.gsub(/\)[5]/, ')*5')
+
+		# self.eq_as_string.gsub(/\)[6]/, ')*6')
+
+		# self.eq_as_string.gsub(/\)[7]/, ')*7')
+
+		# self.eq_as_string.gsub(/\)[8]/, ')*8')
+
+		# self.eq_as_string.gsub(/\)[9]/, ')*9')
+
+		# self.eq_as_string.gsub(/\)[0]/, ')*0')
+
+		# if self.eq_as_string.gsub(/\)\d/, ')*1')
+
+		# 	self.eq_as_string.gsub(/\)\d/, ')*1')
+
+		# elsif self.eq_as_string.gsub(/\)\d/, ')*1')
+
+		# 	self.eq_as_string.gsub(/\)\d/, ')*1')
+			
+
+		# end
+
+		# self.eq_as_string.gsub(/\d\(/, //)
+
+		# split_eq = self.eq_as_string.split('')
+
+		# lastArrIndex = split_eq.length - 1
+
+		# values_in_between_split_eq = split_eq[1...lastArrIndex]
+
+		# values_in_between_split_eq.each do |item|
+
+		# 	next_item = values_in_between_split_eq.index(item) + 1 
+
+		# 	puts values_in_between_split_eq(next_item)
+
+		# 	if item == ")" &&  values_in_between_split_eq(next_item) == "("
+
+		# 		puts 'hi'
+
+		# 	end
+
+		end
+		
+		# if split_eq.first == '('
+
+		# 	puts split_eq.first
+
+		# end
+
+		# split_eq.each do |item|
+
+		# 	# puts item
+
+		# 	if item == "("
+
+		# 		puts split_eq.index(item)
+
+		# 	end 
+
+		# 	# if split_e.index('(') != 0q
+		# 	# 	puts 'hello'
+		# 	# else 
+		# 	# 	 puts 'hi'
+		# 	# end
+
+		# 	nexArrItem = split_eq.index(item) + 1
+
+		# 	# puts split_eq[nexArrItem]
+
+		# end
+
+		# split_eq.map.with_index do |char, i| 
+
+		# 	puts nexArrItem = split_eq.index(char) + 1
+
+		# 	if char == "(" && split_eq[0] == "("
+		# 		# do nothing				
+		# 		# puts char
+		# 	elsif char == ")" && split_eq[lastArrIndex]
+
+		# 	end
+
+		# end
+
 
 	# end
 
-	# def split_string_for_multipliers_and_dividers
+	# def split_string_for_parantheses(eq)
+
+	# 	#split the equation and put everything into array
+
+	# 	#because we first we need to find which parts of the equation
+	# 	#are in parantheses
+
+	# 	#regex is looking for parathenses, lazily, and abstracting everything in it until the next parantheses
+
+	# 	eq_as_array = eq.split(%r{\((.+?)\)})
+	# 	map_array(eq_as_array)
 
 	# end
 
-	# def split_string_for_addition_and_subtraction
+	# # def split_string_for_exponents
 
-	# end
+	# # end
 
-	def map_array(array)
+	# # def split_string_for_multipliers_and_dividers
 
-		array.map do |piece_of_equation_string|
+	# # end
+
+	# # def split_string_for_addition_and_subtraction
+
+	# # end
+
+	# def map_array(array)
+
+	# 	array.map do |piece_of_equation_string|
 
 
-			pieces_of_equation_array = piece_of_equation_string.split('')
+	# 		pieces_of_equation_array = piece_of_equation_string.split('')
 
-			pieces_of_equation_array.map do |piece_of_array|
+	# 		pieces_of_equation_array.map do |piece_of_array|
 
-				if piece_of_array == "*"
+	# 			if piece_of_array == "*"
 
-					piece_of_array
+	# 				piece_of_array
 
-					op = piece_of_array
+	# 				op = piece_of_array
 
-					puts 2.send(op,3)
+	# 				puts 2.send(op,3)
 
 					
 				
-				# elsif piece_of_array == Fixnum
+	# 			# elsif piece_of_array == Fixnum
 
-				# 	firstnum = piece_of_array.to_i
+	# 			# 	firstnum = piece_of_array.to_i
 					
-				end
+	# 			end
 
 
-			end
-
-
-
-			# if item == "+" || "*" || "/" || "-" || "%"
-			# 	puts item
-
-
-		end
-
-	end
-
-	def add(op)
+	# 		end
 
 
 
-	end
-
-	def calc(op)
-
-		#this needs to fire for each evaluation
-		#so when (2 + 3) goes, it returns a value
-		#that can go back into this equation
+	# 		# if item == "+" || "*" || "/" || "-" || "%"
+	# 		# 	puts item
 
 
+	# 	end
 
-		firstnum = a.to_i
-		secondnum = b.to_i
+	# end
 
-		firstnum.send(op,secondnum)
+	# def add(op)
 
-	end
+
+
+	# end
+
+	# def calc(op)
+
+	# 	#this needs to fire for each evaluation
+	# 	#so when (2 + 3) goes, it returns a value
+	# 	#that can go back into this equation
+
+
+
+	# 	firstnum = a.to_i
+	# 	secondnum = b.to_i
+
+	# 	firstnum.send(op,secondnum)
+
+	# end
 
 end 
 
-cal = Calculator.new("2+3(5%4)")
+cal = Calculator.new
 
 
